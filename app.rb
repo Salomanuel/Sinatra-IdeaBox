0f6549a412ea909ff3608997e86b1109bc0e0374
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-03 22:53:10 +0100

    add erb view file

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ Bundler.require
 
 class IdeaBoxApp < Sinatra::Base
 	get '/' do
-		"<blockquote>I guess I always felt even if the world came to an end, McDonald's would still be open. <cite>Susan Beth Pfeffer</cite></blockquote>
-		<h1>Bella li'</h1>"
+		erb :index
+		# "<blockquote>I guess I always felt even if the world came to an end, McDonald's would still be open. <cite>Susan Beth Pfeffer</cite></blockquote>
+		# <h1>Bella li'</h1>"
 	end
 end
