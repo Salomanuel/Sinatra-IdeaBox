f27fae2f14f199a86286b960d072434ea58de798
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-04 13:44:57 +0100

    saving Ideas works from the browser

--- app.rb
+++ app.rb
@@ -7,13 +7,14 @@ Bundler.require
 
 class IdeaBoxApp < Sinatra::Base
 	get '/' do
+		@ideas = Idea.all
 		erb :index
 		# "<blockquote>I guess I always felt even if the world came to an end, McDonald's would still be open. <cite>Susan Beth Pfeffer</cite></blockquote>
 		# <h1>Bella li'</h1>"
 	end
 
 	post '/' do
-		idea = Idea.new
+		idea = Idea.new(params['idea_title'],params['idea_description'])
 		idea.save
 		"Creating an IDEA!"
 	end
