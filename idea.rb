cdeafc8045d630ef7c582900116daec110401a7f
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-04 13:04:08 +0100

    refactor saving into the database

--- idea.rb
+++ idea.rb
@@ -1,10 +1,15 @@
 require 'yaml/store'
 
 class Idea
+	def initialize(title,description)
+		@title 				= title
+		@description 	= description
+	end
+
 	def save
 		database.transaction do |db|
 			db['ideas'] ||= []
-			db['ideas'] << {title: 'diet', description: "pizza all the time"}
+			db['ideas'] << {title: @title, description: @description}
 		end
 	end
 
